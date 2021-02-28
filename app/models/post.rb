ef1943e559f71410760d28c4a696ed2e93e5bf06
Zengo-Master <8435740@gmail.com>
2021-03-01 01:06:53 +0900

    idea

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  # validates :name, presence: true
-  # validates :message, presence: true
+  validates :name, presence: true
+  validates :message, presence: true
 end
